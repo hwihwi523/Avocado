36c5b61cc0471a669825dae3a0cda74b9a4e4591
윤재휘 <yjh200523@naver.com>
2023-04-28 09:59:02 +0900

    S08P31A506-132 [feat] create entity

---
+++ backend/product/src/main/java/com/avocado/product/entity/Store.java
@@ -0,0 +1,39 @@
+package com.avocado.product.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store {
+    // Member 서버로부터 받아 와 저장하므로 식별자 생성 전략은 사용하지 않기
+    @Id
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID providerId;
+
+    // 스토어 이름
+    @Column(length = 25)
+    private String name;
+
+    // 로고 이미지 URL
+    private String logoImgurl;
+
+    // 배너 이미지 URL
+    private String bannerImgurl;
+
+    @Builder
+    public Store(UUID providerId, String name, String logoImgurl, String bannerImgurl) {
+        this.providerId = providerId;
+        this.name = name;
+        this.logoImgurl = logoImgurl;
+        this.bannerImgurl = bannerImgurl;
+    }
+}
