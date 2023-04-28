d84d48a762cfe64bf42a72e39530dca540938997
윤재휘 <yjh200523@naver.com>
2023-04-28 21:29:05 +0900

    S08P31A506-134 [feat] add Common, Cart ErrorCode

--- backend/product/src/main/java/com/avocado/product/exception/ErrorCode.java
+++ backend/product/src/main/java/com/avocado/product/exception/ErrorCode.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    // Common
+    INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE"),
+
     // Consumer
     NO_MEMBER(404, "NO_MEMBER"),
 
@@ -15,6 +18,9 @@ public enum ErrorCode {
     // Wishlist
     EXISTS_WISHLIST(400, "EXISTS_WISHLIST"),
     NO_WISHLIST(404, "NO_WISHLIST"),
+
+    // Cart
+    NO_CART(404, "NO_CART"),
     ;
 
     final int status;
