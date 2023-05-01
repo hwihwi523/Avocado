bd4940535d07005aa00508eb90fbce44411c5c12
윤재휘 <yjh200523@naver.com>
2023-05-01 11:11:45 +0900

    S08P31A506-134 [feat] add data (merchandise_id, score)

--- backend/product/src/main/java/com/avocado/product/dto/query/CartDTO.java
+++ backend/product/src/main/java/com/avocado/product/dto/query/CartDTO.java
@@ -5,19 +5,23 @@ import lombok.Getter;
 
 @Getter
 public class CartDTO {
-    private Long id;
+    private Long cart_id;
     private String brand_name;
+    private Long merchandise_id;
     private String merchandise_name;
     private Integer price;
     private Integer discounted_price;
-//    private Float score;
+    private Float score;
 
     @QueryProjection
-    public CartDTO(Long id, String brand_name, String merchandise_name, Integer price, Integer discounted_price) {
-        this.id = id;
+    public CartDTO(Long cart_id, String brand_name, Long merchandise_id, String merchandise_name,
+                   Integer price, Integer discounted_price, Float score) {
+        this.cart_id = cart_id;
         this.brand_name = brand_name;
+        this.merchandise_id = merchandise_id;
         this.merchandise_name = merchandise_name;
         this.price = price;
         this.discounted_price = discounted_price;
+        this.score = score != null ? score : 0;
     }
 }
