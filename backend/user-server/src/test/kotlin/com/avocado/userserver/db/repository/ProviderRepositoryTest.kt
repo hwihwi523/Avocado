380dc01eaaa109175cb9f34e3d8d4e6a0874e225
hjlim7831 <hjlim7831@gmail.com>
2023-05-01 20:51:19 +0900

    S08P31A506-125 [test] Provider Login Test 추가

--- backend/user-server/src/test/kotlin/com/avocado/userserver/db/repository/ProviderRepositoryTest.kt
+++ backend/user-server/src/test/kotlin/com/avocado/userserver/db/repository/ProviderRepositoryTest.kt
@@ -1,23 +1,18 @@
-package com.avocado.userserver.api.service
+package com.avocado.userserver.db.repository
 
 import com.avocado.userserver.db.entity.Provider
-import com.avocado.userserver.db.repository.ProviderRepository
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
-// TIP : 테스트 코드에서 함수를 private 처리를 하면, test 를 못 찾는다...
-@WebFluxTest(ProviderService::class)
+@WebFluxTest(ProviderRepository::class)
 @ExtendWith(SpringExtension::class)
-internal class ProviderServiceImplTest @Autowired constructor(
-    private val providerService: ProviderService,
+class ProviderRepositoryTest @Autowired constructor(
     private val providerRepository: ProviderRepository
 ) {
-
     @Test
     fun `조회 테스트`() {
         runBlocking {
