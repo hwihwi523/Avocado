e243f95ee027f89c41953ee429e2bb6d1637701c
kdw3238 <kdw3238@naver.com>
2023-05-04 13:31:42 +0900

    08P31A506-174-be-commercial-setting[feat] commercial 서버  초기 설정

---
+++ backend/commercial/src/test/java/com/avocado/commercial/CommercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocado.commercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
