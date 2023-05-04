7a1b859e7e8079b07fe46ce856a1ba697d42b911
hjlim7831 <hjlim7831@gmail.com>
2023-05-04 22:26:21 +0900

    S08P31A506-170 [fix] 회원정보 수정 API 수정
    request 형식 snake case 로 변경
    ageGroup 10 ~ 70 까지의 숫자만 받을 수 있도록 변경

--- backend/user-server/src/main/kotlin/com/avocado/userserver/api/request/ConsumerNotRequiredInfoReq.kt
+++ backend/user-server/src/main/kotlin/com/avocado/userserver/api/request/ConsumerNotRequiredInfoReq.kt
@@ -1,5 +1,9 @@
 package com.avocado.userserver.api.request
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class ConsumerNotRequiredInfoReq(
     val height:Int?,
     val weight:Int?,
