3a880486b28553177c9e4bd0a39634a329842a14
김범식 <kbg7635@naver.com>
2023-05-04 14:59:58 +0900

    S08P31A506-201 [feat] 상품 품목 그리드 컴포넌트 구성

--- frontend/src/components/oranisms/ProductCardsGrid.tsx
+++ frontend/src/components/oranisms/ProductCardsGrid.tsx
@@ -1,32 +1,7 @@
+
 import styled from "@emotion/styled";
-import { useRouter } from "next/router";
-import { Stack } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Chip from "@mui/material/Chip";
-import Image from "next/image";
-import Rating from "@mui/material/Rating";
-
-import { useState } from "react";
-
-import { Pie, Doughnut } from "react-chartjs-2";
-import { Bubble } from "react-chartjs-2";
-import { IconButton } from "@mui/material";
-import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
-import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined";
-import { BlockText, InlineText } from "../components/atoms";
-import { ProductDetailImage, ProductCardsRow } from "../components/oranisms";
-import { ChartPersonalColor } from "../components/oranisms/charts";
-
-import FormControl from "@mui/material";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
-import { useRef } from "react";
-import { ReviewInput, Review } from "../components/molecues";
-
-
-import {ProductCard} from "../components/molecues";
+import {ProductCard} from "../molecues";
 
 type ProductInfo = {
   id: number;
