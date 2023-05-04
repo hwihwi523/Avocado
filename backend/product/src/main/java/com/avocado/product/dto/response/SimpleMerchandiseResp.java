898d10eff0e50f022c9f36fdf212d819951faecc
윤재휘 <yjh200523@naver.com>
2023-05-04 15:21:10 +0900

    S08P31A506-181 [refactor] 불필요 메서드 삭제

--- backend/product/src/main/java/com/avocado/product/dto/response/SimpleMerchandiseResp.java
+++ backend/product/src/main/java/com/avocado/product/dto/response/SimpleMerchandiseResp.java
@@ -10,23 +10,9 @@ public class SimpleMerchandiseResp extends DefaultMerchandiseResp {
     private String image_url;
     private Float score;
 
-    private String mbti;
-    private String personal_color;
-    private String age_group;
-
     public void updateSimple(SimpleMerchandiseDTO simpleMerchandiseDTO) {
         super.updateDefault(simpleMerchandiseDTO);
         this.image_url = simpleMerchandiseDTO.getImageUrl();
         this.score = simpleMerchandiseDTO.getScore() != null ? simpleMerchandiseDTO.getScore() : 0;
-    }
-
-    public void updateMBTI(String mbti) {
-        this.mbti = mbti;
-    }
-    public void updatePersonalColor(String personal_color) {
-        this.personal_color = personal_color;
-    }
-    public void updateAgeGroup(String ageGroup) {
-        this.age_group = ageGroup + "대";
     }
 }
