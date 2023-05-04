414c277c2c2a78214e9d8f33b347db5798c425fe
윤재휘 <yjh200523@naver.com>
2023-05-04 15:19:59 +0900

    S08P31A506-181 [refactor] last_time -> last_date 필드명 변경

--- backend/product/src/main/java/com/avocado/product/dto/response/PageResp.java
+++ backend/product/src/main/java/com/avocado/product/dto/response/PageResp.java
@@ -10,22 +10,22 @@ public class PageResp {
     private final Object content;
     private final Boolean is_last_page;
     private final Long last_id;
-    private final LocalDateTime last_time;
+    private final LocalDateTime last_date;
 
-    public static PageResp of(Object content, Boolean is_last_page, Long last_id, LocalDateTime last_time) {
+    public static PageResp of(Object content, Boolean is_last_page, Long last_id, LocalDateTime last_date) {
         return PageResp.builder()
                 .content(content)
                 .is_last_page(is_last_page)
                 .last_id(last_id)
-                .last_time(last_time)
+                .last_date(last_date)
                 .build();
     }
 
     @Builder
-    private PageResp(Object content, Boolean is_last_page, Long last_id, LocalDateTime last_time) {
+    private PageResp(Object content, Boolean is_last_page, Long last_id, LocalDateTime last_date) {
         this.content = content;
         this.is_last_page = is_last_page;
         this.last_id = last_id;
-        this.last_time = last_time;
+        this.last_date = last_date;
     }
 }
