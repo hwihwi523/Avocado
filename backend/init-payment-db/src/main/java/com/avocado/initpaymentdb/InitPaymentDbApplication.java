332a4e59fbc62f91cd3267b97086d6132dcd2865
HwiHwi <yjh200523@naver.com>
2023-05-06 03:48:00 +0900

    S08P31A506-177 [feat] 결제 서버 DB 생성용 프로젝트 초기화

---
+++ backend/init-payment-db/src/main/java/com/avocado/initpaymentdb/InitPaymentDbApplication.java
@@ -0,0 +1,13 @@
+package com.avocado.initpaymentdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitPaymentDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitPaymentDbApplication.class, args);
+    }
+
+}
