281cc9612e5661dc9852cb0650b6af098800cfc1
HwiHwi <yjh200523@naver.com>
2023-05-08 04:12:22 +0900

    S08P31A506-177 [feat] 카카오페이 API 요청 관련 Config, Util

--- backend/payment/src/main/java/com/avocado/payment/config/WebConfig.java
+++ backend/payment/src/main/java/com/avocado/payment/config/WebConfig.java
@@ -1,6 +1,11 @@
 package com.avocado.payment.config;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +15,22 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*");
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectionRequestTimeout(5000);
+        factory.setReadTimeout(5000);
+
+        // Connection Pool 설정
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(5)
+                .build();
+
+        factory.setHttpClient(httpClient);
+
+        return new RestTemplate(factory);
     }
 }
