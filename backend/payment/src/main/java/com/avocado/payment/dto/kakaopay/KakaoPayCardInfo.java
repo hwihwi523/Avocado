048f4a06fcd3790f2765ee667a0cee3063776474
윤재휘 <yjh200523@naver.com>
2023-05-08 17:11:13 +0900

    S08P31A506-258 [feat] 결제 승인 요청 Response DTO

---
+++ backend/payment/src/main/java/com/avocado/payment/dto/kakaopay/KakaoPayCardInfo.java
@@ -0,0 +1,28 @@
+package com.avocado.payment.dto.kakaopay;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class KakaoPayCardInfo {
+    private String purchase_corp;
+    private String purchase_corp_code;
+    private String issuer_corp;
+    private String issuer_corp_code;
+    private String kakaopay_purchase_corp;
+    private String kakaopay_purchase_corp_code;
+    private String kakaopay_issuer_corp;
+    private String kakaopay_issuer_corp_code;
+    private String bin;
+    private String card_type;
+    private String install_month;
+    private String approved_id;
+    private String card_min;
+    private String interest_free_install;
+    private String card_item_code;
+}
