7d2fbe38a9c6206fd2285f68dfb2ccd30d413cc0
김범식 <kbg7635@naver.com>
2023-05-08 16:03:54 +0900

    S08P31A506-297 [feat] 결제 정보 컴포넌트 생성

---
+++ frontend/src/components/molecues/AddressInput.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@mui/material';
+import DaumPostcode from 'react-daum-postcode';
+
+const AddressInput: React.FC<{
+  setVisible: (data: boolean) => void;
+  dataRef:(data:string)=>void;
+}> = (props) => {
+  const handleComplete = (data: any) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+    props.dataRef(fullAddress);
+    props.setVisible(false);
+  };
+
+  return (
+    <>
+      <div>
+        <DaumPostcode onComplete={handleComplete} />
+        <Button
+          sx={{ mt: 3 }}
+          fullWidth
+          onClick={() => {
+            props.setVisible(false);
+          }}
+          variant="outlined"
+          color="error"
+        >
+          close
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default AddressInput;
