ac73b6841a0a88f9f77d7f72e70be573535342aa
HwiHwi <yjh200523@naver.com>
2023-05-09 01:48:28 +0900

    S08P31A506-266 [refactor] 구매내역 테이블 분리

---
+++ backend/product/src/main/java/com/avocado/product/entity/PurchasedMerchandise.java
@@ -0,0 +1,54 @@
+package com.avocado.product.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PurchasedMerchandise {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 거래된 상품
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "merchandise_id")
+    private Merchandise merchandise;
+
+    // 판매자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "provider_id")
+    private Store provider;
+
+    // 구매내역
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchase_id")
+    private Purchase purchase;
+
+    // 수량
+    private Integer quantity;
+
+    // 가격
+    private Long price;
+
+    // 사이즈
+    @Column(columnDefinition = "VARCHAR(10)")
+    private String size;
+
+    @Builder
+    public PurchasedMerchandise(Long id, Merchandise merchandise, Store provider, Purchase purchase, Integer quantity,
+                                Long price, String size, LocalDateTime createdAt) {
+        this.id = id;
+        this.merchandise = merchandise;
+        this.provider = provider;
+        this.purchase = purchase;
+        this.quantity = quantity;
+        this.price = price;
+        this.size = size;
+    }
+}
