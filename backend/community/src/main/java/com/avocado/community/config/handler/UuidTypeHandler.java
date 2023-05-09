d04929bcf4544ac50697bcc05a60fbaad118ad99
hjlim7831 <hjlim7831@gmail.com>
2023-05-09 10:49:06 +0900

    S08P31A506-255 [chore] UuidTypeHandler 로그 추가

--- backend/community/src/main/java/com/avocado/community/config/handler/UuidTypeHandler.java
+++ backend/community/src/main/java/com/avocado/community/config/handler/UuidTypeHandler.java
@@ -1,5 +1,6 @@
 package com.avocado.community.config.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
@@ -12,13 +13,16 @@ import java.sql.SQLException;
 import java.util.UUID;
 
 @MappedJdbcTypes(JdbcType.BINARY)
+@Slf4j
 public class UuidTypeHandler extends BaseTypeHandler<UUID> {
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
+        log.info("setNotNullParameter - uuid: {}", parameter);
         ps.setBytes(i, uuidToBytes(parameter));
     }
 
     private static byte[] uuidToBytes(UUID uuid) {
+        log.info("uuidToBytes - uuid: {}", uuid);
         ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
         bb.putLong(uuid.getMostSignificantBits());
         bb.putLong(uuid.getLeastSignificantBits());
@@ -41,12 +45,16 @@ public class UuidTypeHandler extends BaseTypeHandler<UUID> {
     }
 
     private static UUID bytesToUuid(byte[] bytes) {
+        log.info("bytesToUuid - bytes: {}", bytes);
         if (bytes == null) {
             return null;
         }
         ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
         Long high = byteBuffer.getLong();
         Long low = byteBuffer.getLong();
-        return new UUID(high, low);
+
+        UUID uuid = new UUID(high, low);
+        log.info("uuid: {}", uuid);
+        return uuid;
     }
 }
