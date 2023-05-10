467b8b25c351c1f00fa2ebf56d498b7266d73e5e
hjlim7831 <hjlim7831@gmail.com>
2023-05-10 17:15:56 +0900

    S08P31A506-230 [feat] 통계 서버 생성 및 기본 환경 세팅

---
+++ backend/statistics/src/main/java/com/avocado/statistics/common/error/BaseException.java
@@ -0,0 +1,18 @@
+package com.avocado.statistics.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseException extends RuntimeException {
+    public HttpStatus status;
+    public String msg;
+
+    public BaseException(ResponseCode code) {
+        this.status = code.status;
+        this.msg = code.msg;
+    }
+
+}
