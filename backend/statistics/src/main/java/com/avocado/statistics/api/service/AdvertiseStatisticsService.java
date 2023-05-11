25d4584bf3b1a2c97d3a03f403b361958740db1b
hjlim7831 <hjlim7831@gmail.com>
2023-05-12 08:11:02 +0900

    S08P31A506-230 [feat] 광고 데이터 처리 과정 구현

--- backend/statistics/src/main/java/com/avocado/statistics/api/service/AdvertiseStatisticsService.java
+++ backend/statistics/src/main/java/com/avocado/statistics/api/service/AdvertiseStatisticsService.java
@@ -1,14 +1,57 @@
 package com.avocado.statistics.api.service;
 
+import com.avocado.statistics.common.utils.DateUtil;
 import com.avocado.statistics.db.redis.repository.AdvertiseCountRepository;
+import com.avocado.statistics.kafka.dto.AdStatus;
+import com.avocado.statistics.kafka.dto.Status;
+import com.avocado.statistics.kafka.dto.Type;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class AdvertiseStatisticsService {
 
     private final AdvertiseCountRepository advertiseCountRepository;
+    private final DateUtil dateUtil;
+
+
+    public void sendAdvertiseInfo() {
+        String nowDate = dateUtil.getNowDate();
+        List<Status> statusList = new ArrayList<>();
+
+        Map<Long, Integer> viewMap = advertiseCountRepository.getMap(Type.AD_VIEW, nowDate);
+        Map<Long, Integer> clickMap = advertiseCountRepository.getMap(Type.AD_CLICK, nowDate);
+        Map<Long, Integer> payMap = advertiseCountRepository.getMap(Type.AD_PAYMENT, nowDate);
+
+
+        Set<Long> merchandiseIds = new HashSet<>();
+        merchandiseIds.addAll(viewMap.keySet());
+        merchandiseIds.addAll(clickMap.keySet());
+        merchandiseIds.addAll(payMap.keySet());
+
+        for (Long merchandiseId: merchandiseIds) {
+            Integer exposureCnt = viewMap.getOrDefault(merchandiseId, 0);
+            Integer clickCnt = clickMap.getOrDefault(merchandiseId, 0);
+            Integer quantity = payMap.getOrDefault(merchandiseId, 0);
+
+            Status status = Status.builder()
+                    .merchandiseId(merchandiseId)
+                    .exposureCnt(exposureCnt)
+                    .clickCnt(clickCnt)
+                    .quantity(quantity).build();
+            statusList.add(status);
+        }
+        
+        // Kafka 로 보내기 (to commercial Server)
+        AdStatus adStatus = AdStatus.builder()
+                .date(nowDate)
+                .statusList(statusList)
+                .build();
+        System.out.println(adStatus);
+    }
 
 
 
