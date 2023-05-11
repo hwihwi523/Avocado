53035a113fed32a22ea31165e4d4e41fe4859c8f
윤재휘 <yjh200523@naver.com>
2023-05-11 15:39:37 +0900

    S08P31A506-289 [fix] Jwt 만료 예외

--- backend/payment/src/main/java/com/avocado/payment/config/GlobalExceptionHandler.java
+++ backend/payment/src/main/java/com/avocado/payment/config/GlobalExceptionHandler.java
@@ -1,10 +1,7 @@
 package com.avocado.payment.config;
 
 import com.avocado.payment.dto.response.BaseResp;
-import com.avocado.payment.exception.ErrorCode;
-import com.avocado.payment.exception.InvalidValueException;
-import com.avocado.payment.exception.KakaoPayException;
-import com.avocado.payment.exception.NoInventoryException;
+import com.avocado.payment.exception.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +12,20 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessLogicException.class)
+    protected ResponseEntity<BaseResp> handleRedirectBusinessLogicException(BusinessLogicException e) {
+        log.error(e.getMessage() + " >> " + e.getErrorCode().getMessage());
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity
+                .status(HttpStatus.valueOf(errorCode.getStatus()))
+                .body(BaseResp.of(errorCode.getMessage()));
+    }
+    @ExceptionHandler(RedirectBusinessLogicException.class)
+    protected RedirectView handleRedirectBusinessLogicException(RedirectBusinessLogicException e) {
+        log.error(e.getMessage() + " >> " + e.getErrorCode().getMessage());
+        return new RedirectView("http://k8s-avocado-c6c0ca3c6e-209771398.ap-northeast-2.elb.amazonaws.com");
+    }
+
     /**
      * 재고 없을 경우 재고 없음 페이지로 이동
      */
