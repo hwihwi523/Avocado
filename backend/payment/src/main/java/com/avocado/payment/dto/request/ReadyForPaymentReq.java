b04df2d687958dd1c491ab166cbd5ba6bd57fedd
윤재휘 <yjh200523@naver.com>
2023-05-11 15:40:13 +0900

    S08P31A506-289 [refactor] Req DTO에서 user_id 삭제

--- backend/payment/src/main/java/com/avocado/payment/dto/request/ReadyForPaymentReq.java
+++ backend/payment/src/main/java/com/avocado/payment/dto/request/ReadyForPaymentReq.java
@@ -7,13 +7,11 @@ import java.util.List;
 
 @Getter
 public class ReadyForPaymentReq {
-    private final String user_id;  // 로그인 구현 시 삭제 예정
     private final Long total_price;
     private final List<PurchaseMerchandiseReq> merchandises;
 
     @Builder
-    public ReadyForPaymentReq(String user_id, Long total_price, List<PurchaseMerchandiseReq> merchandises) {
-        this.user_id = user_id;
+    public ReadyForPaymentReq(Long total_price, List<PurchaseMerchandiseReq> merchandises) {
         this.total_price = total_price;
         this.merchandises = merchandises;
     }
