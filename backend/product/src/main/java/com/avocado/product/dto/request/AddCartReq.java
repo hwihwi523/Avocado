e8587f0abf7502c31dc5a0fb27353ee80abae25d
윤재휘 <yjh200523@naver.com>
2023-05-11 16:07:21 +0900

    S08P31A506-289 [feat] 토큰 파싱 로직 추가 (장바구니)

--- backend/product/src/main/java/com/avocado/product/dto/request/AddCartReq.java
+++ backend/product/src/main/java/com/avocado/product/dto/request/AddCartReq.java
@@ -2,15 +2,15 @@ package com.avocado.product.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class AddCartReq {
-    private final String user_id;
-    private final Long merchandise_id;
+    private Long merchandise_id;
 
     @Builder
-    public AddCartReq(String user_id, Long merchandise_id) {
-        this.user_id = user_id;
+    public AddCartReq(Long merchandise_id) {
         this.merchandise_id = merchandise_id;
     }
 }
