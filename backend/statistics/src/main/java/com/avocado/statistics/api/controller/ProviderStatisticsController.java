dc8453edc3ab4ed47db573b4543bfd9b11a56c20
hjlim7831 <hjlim7831@gmail.com>
2023-05-12 09:02:00 +0900

    S08P31A506-245 [feat] 구매자 컨트롤러 구현

--- backend/statistics/src/main/java/com/avocado/statistics/api/controller/ProviderStatisticsController.java
+++ backend/statistics/src/main/java/com/avocado/statistics/api/controller/ProviderStatisticsController.java
@@ -1,7 +1,25 @@
 package com.avocado.statistics.api.controller;
 
+import com.avocado.statistics.api.service.ProviderStatisticsService;
+import com.avocado.statistics.common.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@RequiredArgsConstructor
 public class ProviderStatisticsController {
+
+    private final JwtUtils jwtUtils;
+    private final ProviderStatisticsService providerStatisticsService;
+
+    @GetMapping("/provider")
+    public ResponseEntity<?> getProviderStatisticsInfo(HttpServletRequest request) {
+        Claims claims = jwtUtils.getClaims(request);
+        return ResponseEntity.ok(providerStatisticsService.getStatisticsInfo(claims));
+    }
 }
