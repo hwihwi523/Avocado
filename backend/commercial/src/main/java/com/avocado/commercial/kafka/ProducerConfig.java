60d5137193df7ede220ea3916f8a83d94b750087
인덕 <indeokiya@snu.ac.kr>
2023-05-12 13:56:00 +0900

    S08P31A506-159-streaming [fix] kafka config 수정

--- backend/commercial/src/main/java/com/avocado/commercial/kafka/ProducerConfig.java
+++ backend/commercial/src/main/java/com/avocado/commercial/kafka/ProducerConfig.java
@@ -4,6 +4,7 @@ import com.avocado.commercial.Adview;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -11,6 +12,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class ProducerConfig {
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
