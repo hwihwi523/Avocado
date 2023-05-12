24a4dcddafd9f57bf1cbb8c060fa71b2a9be6b74
윤재휘 <yjh200523@naver.com>
2023-05-12 15:29:03 +0900

    S08P31A506-278 [feat] JPA entity 추가

---
+++ backend/statistics/src/main/java/com/avocado/statistics/db/mysql/entity/jpa/Color.java
@@ -0,0 +1,32 @@
+package com.avocado.statistics.db.mysql.entity.jpa;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Color {
+    @Id
+    private Long id;
+
+    // 색상명 (한국어)
+    @Column(length = 30)
+    private String nameKor;
+
+    // 색상명 (영어)
+    @Column(length = 30)
+    private String nameEng;
+
+//    @Builder
+//    public Color(Long id, String nameKor, String nameEng) {
+//        this.id = id;
+//        this.nameKor = nameKor;
+//        this.nameEng = nameEng;
+//    }
+}
---
+++ backend/statistics/src/main/java/com/avocado/statistics/db/mysql/entity/jpa/Mbti.java
@@ -0,0 +1,29 @@
+package com.avocado.statistics.db.mysql.entity.jpa;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mbti")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Mbti {
+    @Id
+    private Byte id;
+
+    // MBTI 종류
+    @Column(columnDefinition = "CHAR(4)")
+    private String kind;
+
+//    @Builder
+//    public MBTI(Byte id, String kind) {
+//        this.id = id;
+//        this.kind = kind;
+//    }
+}
