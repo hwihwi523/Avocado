5c79efbc307db9055173e85028879d33b8654255
윤재휘 <yjh200523@naver.com>
2023-05-12 11:10:13 +0900

    S08P31A506-167 [refactor] 결제 요청 정보 DTO 사이즈 추가

--- backend/payment/src/main/java/com/avocado/payment/dto/request/PurchaseMerchandiseReq.java
+++ backend/payment/src/main/java/com/avocado/payment/dto/request/PurchaseMerchandiseReq.java
@@ -10,11 +10,13 @@ public class PurchaseMerchandiseReq {
     private final Long merchandise_id;
     private final Integer quantity;
     private final Long price;
+    private final String size;
 
     @Builder
-    public PurchaseMerchandiseReq(Long merchandise_id, Integer quantity, Long price) {
+    public PurchaseMerchandiseReq(Long merchandise_id, Integer quantity, Long price, String size) {
         this.merchandise_id = merchandise_id;
         this.quantity = quantity;
         this.price = price;
+        this.size = size;
     }
 }
