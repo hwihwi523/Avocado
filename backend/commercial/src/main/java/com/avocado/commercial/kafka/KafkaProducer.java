689951b6a564e69c002ac0a32e1db0dea68f5c55
인덕 <indeokiya@snu.ac.kr>
2023-05-12 17:48:51 +0900

    S08P31A506-159-streaming [feat] click, adview event producing 추가

--- backend/commercial/src/main/java/com/avocado/commercial/kafka/KafkaProducer.java
+++ backend/commercial/src/main/java/com/avocado/commercial/kafka/KafkaProducer.java
@@ -27,7 +27,9 @@ public class KafkaProducer {
     }
 
     public void sendAdview(Long adId, UUID userId) {
-        Adview adview = Adview.newBuilder().setUserId(userId.toString()).build();
+        Adview adview = Adview.newBuilder()
+                .setUserId(makeRandomUUID().toString())
+                .build();
         ListenableFuture<SendResult<Long, Adview>> future = adviewKafkaTemplate.send(adTopic, adId, adview);
         future.addCallback(new ListenableFutureCallback<>() {
             @Override
@@ -40,5 +42,34 @@ public class KafkaProducer {
                 log.info("adview sent: [{}] with partition = [{}] offset=[{}]", adview, result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
             }
         });
+    }
+
+    public UUID makeRandomUUID() {
+
+        /*
+        '24E258AF-5B31-4BF1-8268-8F0EFACB1F04', '임혜진'
+        '2D2168DE-D05E-426E-A6A5-B4D7516BAA52', '오현규'
+        '3AC71B16-28E7-4EF4-8438-AFD76F6CEACE', '김도원'
+        'CD9C70A5-76D5-43FF-9A7B-A34A211F3240', '김범식'
+        'F742BD5B-3AC2-4A57-93A5-006719728AB0', '윤재휘'
+        */
+
+        int randomIndex = (int) (Math.random() * 2);
+
+        switch (randomIndex) {
+            case 0:
+                return UUID.fromString("24E258AF-5B31-4BF1-8268-8F0EFACB1F04");
+            case 1:
+                return UUID.fromString("2D2168DE-D05E-426E-A6A5-B4D7516BAA52");
+//            case 2:
+//                return UUID.fromString("3AC71B16-28E7-4EF4-8438-AFD76F6CEACE");
+//            case 3:
+//                return UUID.fromString("CD9C70A5-76D5-43FF-9A7B-A34A211F3240");
+//            case 4:
+//                return UUID.fromString("F742BD5B-3AC2-4A57-93A5-006719728AB0");
+            default:
+                throw new IllegalStateException("Unexpected value: " + randomIndex);
+        }
+
     }
 }
--- backend/commercial/src/main/java/com/avocado/commercial/kafka/ProducerConfig.java
+++ backend/commercial/src/main/java/com/avocado/commercial/kafka/ProducerConfig.java
@@ -1,6 +1,7 @@
 package com.avocado.commercial.kafka;
 
 import com.avocado.commercial.Adview;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +25,8 @@ public class ProducerConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put("schema.registry.url", "http://a5ef82e13fcbc44689f93c4924981608-494875664.ap-northeast-2.elb.amazonaws.com:8081");
 //        props.put(JsonSerializer.TYPE_MAPPINGS,
 //                "bid:com.dokidoki.bid.kafka.dto.KafkaBidDTO");
 
