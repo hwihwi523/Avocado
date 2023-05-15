48a1371088f327a4c39cccc7dd2bc8458c36f8e6
HwiHwi <yjh200523@naver.com>
2023-05-15 22:49:31 +0900

    [refactor] 상품 상세정보 조회 시 판매자 ID 함께 조회

--- backend/product/src/main/java/com/avocado/product/dto/query/DetailMerchandiseDTO.java
+++ backend/product/src/main/java/com/avocado/product/dto/query/DetailMerchandiseDTO.java
@@ -3,17 +3,21 @@ package com.avocado.product.dto.query;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class DetailMerchandiseDTO extends DefaultMerchandiseDTO {
+    private final UUID providerId;
     private final Integer inventory;
     private final Float score;
     private final String description;
 
     @QueryProjection
     public DetailMerchandiseDTO(String brandName, Long merchandiseId, String merchandiseCategory, String imageUrl,
-                                String merchandiseName, Integer price, Integer discountedPrice, Integer inventory,
-                                Float score, String description) {
+                                String merchandiseName, Integer price, Integer discountedPrice, UUID providerId,
+                                Integer inventory, Float score, String description) {
         super(brandName, merchandiseId, merchandiseCategory, imageUrl, merchandiseName, price, discountedPrice);
+        this.providerId = providerId;
         this.inventory = inventory;
         this.score = score;
         this.description = description;
