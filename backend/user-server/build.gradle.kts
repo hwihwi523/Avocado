eb477b0d834ae276fd2053dd04f841c7504ff2a7
hjlim7831 <hjlim7831@gmail.com>
2023-05-17 14:04:48 +0900

    S08P31A506-360 [fix] 사용자 서버 kafka 연결 설정 변경

--- backend/user-server/build.gradle.kts
+++ backend/user-server/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
-    id("com.github.davidmc24.gradle.plugin.avro") version "1.7.1"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.3.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
