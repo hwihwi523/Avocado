3582447211e91b05dbc3b7c3413c0342f13e66c9
ohhyunkyu <uyk0928@gmail.com>
2023-05-23 13:31:46 +0900

    hotfix [fix] 메인화면 로그인 에러 수정

--- frontend/src/pages/login/oauth-redirect.tsx
+++ frontend/src/pages/login/oauth-redirect.tsx
@@ -1,11 +1,9 @@
 import { DecodedToken } from "@/src/features/auth/authApi";
-import { Member, setMember } from "@/src/features/auth/authSlice";
 import { useQueryParams } from "@/src/hooks/useQueryParams";
 import { setToken } from "@/src/utils/tokenManager";
 import jwt from "jsonwebtoken";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 
 const SECRET = process.env.NEXT_PUBLIC_JWT_SECRET
   ? process.env.NEXT_PUBLIC_JWT_SECRET
@@ -13,7 +11,6 @@ const SECRET = process.env.NEXT_PUBLIC_JWT_SECRET
 
 export default function OAuthRedirect() {
   const router = useRouter();
-  const dispatch = useDispatch();
   const queryParams = useQueryParams();
 
   useEffect(() => {
@@ -34,29 +31,13 @@ export default function OAuthRedirect() {
           const refreshExp = decodedRefreshToken.exp;
           setToken("ACCESS_TOKEN", accessToken, accessExp);
           setToken("REFRESH_TOKEN", refreshToken, refreshExp);
-          let member: Member;
-          member = {
-            type: decodedRefreshToken.type,
-            id: decodedRefreshToken.id,
-            email: decodedRefreshToken.email,
-            name: decodedRefreshToken.name,
-            grade: decodedRefreshToken.grade,
-            picture_url: decodedRefreshToken.picture_url,
-            gender: decodedRefreshToken.gender,
-            age_group: decodedRefreshToken.age_group,
-            height: decodedRefreshToken.height,
-            weight: decodedRefreshToken.weight,
-            mbti_id: decodedRefreshToken.mbti_id,
-            personal_color_id: decodedRefreshToken.personal_color_id,
-          };
-          dispatch(setMember(member));
         } catch (err) {
           console.error(err);
           // 토큰 검증에 실패했을 때의 처리 로직
         }
       }
-      // 멤버 로그인 정보 store에 저장 후 리플레이스
-      router.replace("/", undefined, { shallow: true });
+      // 리플레이스(리로드 동반)
+      location.replace("/");
     };
 
     navigateToLogin();
